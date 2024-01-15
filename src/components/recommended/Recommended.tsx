e4614db2f853455b3ba0ab4273fed9db5d19022d
mkihato <mlamaar9@gmail.com>
2024-01-15 21:40:37 +0300

    dos commit

--- src/components/recommended/Recommended.tsx
+++ src/components/recommended/Recommended.tsx
@@ -3,8 +3,49 @@ import "../recommended/recommended.scss";
 const Recommended = () => {
   return (
     <div className="recommended">
-      <h1>Recommended</h1>
-      <p>Recommended goes here</p>
+      <h2>Recommended Artists</h2>
+      <div className="recommendedArtists">
+        <div className="item">
+          <img src="/noavatar.png" alt="" className="icon" />
+          <span>Chris Brown</span>
+        </div>
+        <div className="item">
+          <img src="/noavatar.png" alt="" className="icon" />
+          <span>Chris Brown</span>
+        </div>
+        <div className="item">
+          <img src="/noavatar.png" alt="" className="icon" />
+          <span>Chris Brown</span>
+        </div>
+      </div>
+      <div className="recommendedTracks">
+        <h2>Recommended Tracks</h2>
+        <div className="item">
+          <img src="/noavatar.png" alt="" className="icon" />
+          <span>Troubled waters</span>
+          <span>Chris Brown</span>
+        </div>
+        <div className="item">
+          <img src="/noavatar.png" alt="" className="icon" />
+          <span>Troubled waters</span>
+          <span>Chris Brown</span>
+        </div>
+        <div className="item">
+          <img src="/noavatar.png" alt="" className="icon" />
+          <span>Troubled waters</span>
+          <span>Chris Brown</span>
+        </div>
+        <div className="item">
+          <img src="/noavatar.png" alt="" className="icon" />
+          <span>Troubled waters</span>
+          <span>Chris Brown</span>
+        </div>
+        <div className="item">
+          <img src="/noavatar.png" alt="" className="icon" />
+          <span>Troubled waters</span>
+          <span>Chris Brown</span>
+        </div>
+      </div>
     </div>
   );
 };
