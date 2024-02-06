e011fcfad710d968e5b34795231970d9a7665eb1
mkihato <mlamaar9@gmail.com>
2024-02-06 22:03:33 +0300

    another one

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -6,22 +6,17 @@ const Menu = () => {
     <div className="menu">
       <span className="title">What to do?</span>
       {menu.map((item) => (
-        <div className="item" key={item.id}>
+        <button className="item" key={item.id}>
           <img src={item.icon} alt="" className="icon" />
           <span className="icontitle">{item.title}</span>
-        </div>
+        </button>
       ))}
       <div className="description">
         <div className="descriptionitem">
-          {/* <img src="/view.svg" alt="" className="icon" /> */}
           <span className="description-title">Description</span>
         </div>
         <div className="descriptiontext">
-          {menu.map((item) => (
-            <span className="description-text" key={item.id}>
-              {item.description}
-            </span>
-          ))}
+          <span className="description-text">{}</span>
         </div>
       </div>
     </div>
